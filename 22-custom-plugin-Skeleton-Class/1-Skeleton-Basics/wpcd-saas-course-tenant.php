d853f498b7a56e4db89e9ce38388d8c9bf28ccf4
Nigel Bahadur <elindydotcom@hotmail.com>
2024-02-08 05:57:05 -0600

    Skeleton class basics.

---
+++ 22-custom-plugin-Skeleton-Class/1-Skeleton-Basics/wpcd-saas-course-tenant.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: WPCD Saas Course Custom Functions For Tenant Sites
+Plugin URI: https://wpclouddeploy.com
+Description: Handles support functions needed for template/tenant sites
+Version: 1.0.0
+Item Id:
+Requires PHP: 8.0
+Author: WPCloudDeploy & OpenSaaS.io
+Author URI: https://wpclouddeploy.com
+*/
+
+require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+class Wpcd_Saas_Course_Tenant {
+	
+}
+
+// Initialize the Class.
+$wpcd_saas_course_tenant_throwaway = new Wpcd_Saas_Course_Tenant();
+
